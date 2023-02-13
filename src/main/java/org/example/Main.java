b8c1e6f582e133e4df5af4139a9b6602d9e00e94
Mathieu Cantagrel <mathieu.cantagrel@carbon-it.com>
2023-02-13 15:28:10 +0100

    integrated components in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.domain.DataParser;
+import org.example.domain.Lawn;
+import org.example.exceptions.InvalidDataFormatException;
+import org.example.exceptions.InvalidLawnException;
+import org.example.exceptions.InvalidMowerException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Lawn lawn = DataParser.linesToLawn(Files.readAllLines(Path.of("src/main/resources/input.txt")));
+            lawn.mow();
+            Files.write(Path.of("src/main/resources/output.txt"), DataParser.lawnToLines(lawn));
+        } catch (InvalidMowerException | InvalidDataFormatException | InvalidLawnException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
